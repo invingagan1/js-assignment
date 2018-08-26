8c90063ab5d39e7caccf76a83fbfb562ec90b892
Gagan Deep Singh <Gagandeep.singh@barco.com>
2018-08-26 11:43:50 +0530

    number section modified

--- app/numbers.js
+++ app/numbers.js
@@ -1,22 +1,28 @@
-if (typeof define !== 'function') { var define = require('amdefine')(module); }
+if (typeof define !== 'function') {
+  var define = require('amdefine')(module);
+}
 
-define(function() {
+define(function () {
   return {
-    valueAtBit: function(num, bit) {
+    valueAtBit: function (num, bit) {
 
     },
 
-    base10: function(str) {
-
+    base10: function (str) {
+      return str.split('').reverse().reduce(function (pVal, cVal, index) {
+        return (cVal === '1') ? pVal + Math.pow(2, index) : pVal;
+      }, 0);
     },
 
-    convertToBinary: function(num) {
+    convertToBinary: function (num) {},
 
-    },
-
-    multiply: function(a, b) {
-
+    multiply: function (a, b) {
+      var precisionLength = 0;
+      var b_numArr = b.toString().split('.');
+      if (b_numArr.length > 1) {
+        precisionLength = b_numArr[1].length;
+      }
+      return parseFloat((a * b).toFixed(precisionLength));
     }
   };
 });
-
