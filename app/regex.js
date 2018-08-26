f01a3c5128df80c99d567e0e62407a8944d7a778
Gagan Deep Singh <Gagandeep.singh@barco.com>
2018-08-26 20:49:25 +0530

    regex modified.

--- app/regex.js
+++ app/regex.js
@@ -1,27 +1,31 @@
-if (typeof define !== 'function') { var define = require('amdefine')(module); }
+if (typeof define !== 'function') {
+  var define = require('amdefine')(module);
+}
 
-define(function() {
+define(function () {
   return {
-    containsNumber : function(str) {
+    containsNumber: function (str) {
+      var regex = /[0-9]/;
+      return regex.test(str);
+    },
+
+    containsRepeatingLetter: function (str) {
 
     },
 
-    containsRepeatingLetter : function(str) {
+    endsWithVowel: function (str) {
+      var regex = /[a,e,i,o,u]$/i;
+      return regex.test(str);
+    },
+
+    captureThreeNumbers: function (str) {
 
     },
 
-    endsWithVowel : function(str) {
+    matchesPattern: function (str) {
 
     },
-
-    captureThreeNumbers : function(str) {
-
-    },
-
-    matchesPattern : function(str) {
-
-    },
-    isUSD : function(str) {
+    isUSD: function (str) {
 
     }
   };
