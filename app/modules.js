ae48bebdebf744f4a971a5c926568fb63759ef41
Gagan Deep Singh <Gagandeep.singh@barco.com>
2018-08-26 12:08:08 +0530

    module created.

--- app/modules.js
+++ app/modules.js
@@ -1,10 +1,21 @@
-if (typeof define !== 'function') { var define = require('amdefine')(module); }
+if (typeof define !== 'function') {
+  var define = require('amdefine')(module);
+}
 
-define(function() {
+define(function () {
   return {
-    createModule : function(str1, str2) {
+    createModule: function (str1, str2) {
+      return (function () {
+        // Define private members here;
 
+        return {
+          sayIt: function () {
+            return this.greeting + ', ' + this.name;
+          },
+          name: str2,
+          greeting: str1
+        };
+      })();
     }
   };
 });
-
--- tests/app/modules.js
+++ tests/app/modules.js
@@ -1,14 +1,18 @@
 /*globals describe:true, it:true, expect:true, beforeEach:true */
-if (typeof define !== 'function') { var define = require('amdefine')(module); }
-if (typeof expect !== 'function') { var expect = require('expect.js'); }
+if (typeof define !== 'function') {
+  var define = require('amdefine')(module);
+}
+if (typeof expect !== 'function') {
+  var expect = require('expect.js');
+}
 
 define([
   'app/modules'
-], function(answers) {
-  describe('the module pattern', function() {
-    it('you should be able to create a function that returns a module', function() {
+], function (answers) {
+  describe('the module pattern', function () {
+    it('you should be able to create a function that returns a module', function () {
       var module = answers.createModule('hello', 'matt');
-
+      console.log(module);
       expect(module.sayIt).to.be.a('function');
       expect(module.name).to.eql('matt');
       expect(module.greeting).to.eql('hello');
